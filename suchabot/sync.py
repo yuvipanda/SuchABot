4774f258f9cfe6298378db82f5c460d4a25df9a6
YuviPanda <yuvipanda@gmail.com>
2013-05-22 18:37:05 +0530

    Proper syntax for multiple fetch targets

--- suchabot/sync.py
+++ suchabot/sync.py
@@ -39,7 +39,7 @@ def ensure_repo(name):
     if is_git_repo(clone_folder):
         sh.cd(clone_folder)
         log("Repo found, updating...")
-        sh.git.fetch('origin gerrit')
+        sh.git.fetch('origin', 'gerrit')
         log("Updated!")
     else:
         log("Repo not found. Cloning...")
