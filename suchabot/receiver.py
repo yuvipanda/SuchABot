2cc37cf51125e2d4bee82095c0e7b9f6ea3749b3
Kunal Mehta <legoktm@gmail.com>
2013-05-23 08:49:27 -0700

    Update script to version on server *Fixed fetching the value from cgi *Use the correct repo name *Convert the number to a string *Fix the naming of the arguments

--- suchabot/receiver.py
+++ suchabot/receiver.py
@@ -11,15 +11,14 @@ environ = os.environ
 environ['PATH'] = '/bin:/usr/bin:/usr/local/bin'
 
 form = cgi.FieldStorage()
-payload = json.loads(form['payload'])
-repo = payload['repository']['full_name']
-number = payload['number']
+payload = json.loads(form['payload'].value)
+repo = payload['repository']['name']
+number = str(payload['number'])
 
-script = os.path.expanduser('~/code/SuchABot/suchabot/sync.bash')
 subprocess.check_output('/usr/local/bin/jsub'
                         ' -N suchabot'
                         ' -o /data/project/suchaserver/suchabot.out'
                         ' -e /data/project/suchaserver/suchabot.err'
                         ' -mem 512M /data/project/suchaserver/code/SuchABot/suchabot/sync.bash'
-                        ' {1} {2}'.format(pipes.quote(repo), pipes.quote(number)),
+                        ' {0} {1}'.format(pipes.quote(repo), pipes.quote(number)),
                         stderr=subprocess.STDOUT, shell=True, env=environ)
